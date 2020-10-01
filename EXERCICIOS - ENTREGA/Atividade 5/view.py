6b6f669dd08169fd3201176c85909bad1d8e8b43
unknown <ygorsallesaniceto@gmail.com>
2020-10-01 16:26:15 -0300

    Trab5

--- EXERCICIOS - ENTREGA/Atividade 5/view.py
+++ EXERCICIOS - ENTREGA/Atividade 5/view.py
@@ -1,38 +1,71 @@
 from decimal import *
-from modelo import Order
 from datetime import datetime
 
+
 class View():
-    def __init__(self):
+
+    def inicio(self):
         return self.menu()
 
     def menu(self):
-        print('********MENU*************')
-        print('1 - Cadastrar pedido')
-        print('2 - Deletar pedido')
-        print('1 - Consultar pedido')
-        print('1 - Alterar dados de um pedido')
+        print('--- MENU ---')
+        print('[1] - Cadastrar um produto')
+        print('[2] - Deletar um produto')
+        print('[3] - Consultar um produto')
+        print('[4] - Alterar dados de um produto')
+        #print('[5] - Consultar relatório de pedidos')
+        #print('[6] - Cadastrar uma venda')
+        #print('[7] - Alterar quantidade de produtos vendidos')
+        print('[8] - Sair')
+
         opcao = int(input('Digite a opção desejada: '))
+
         return opcao
-    
-    def coletaDadosPedido(self):
-        orderid = input('Digite identificador do pedido: ')
-        customerid = input('Digite identificador do cliente: ') 
-        employeeid = input('Digite identificador do funcionario: ') 
-        requireddate = input('Digite data requerida: ') 
-        shippedate = input('Digite data shippe: ') 
-        freight = input('Digite o valor de frete: ') 
-        shipname = input('Digite o nome: ')
-        shipaddress = input('Digite endereço: ') 
-        shipcity = input('Digite a cidade: ') 
-        shipregion = input('Digite região: ')
-        shippostalcode = input('Digite cartão postal: ') 
-        shipcountry = input('Digite o país') 
-        shipperid = input('Digite identificador remetente')
-        valores = [orderid, customerid, employeeid, requireddate, shippedate, freight, shipname,
-            shipaddress, shipcity, shipregion, shippostalcode, shipcountry, shipperid]
+
+    def coletadadosproduto(self):
+        productid = input('Digite o identificador do produto: ') 
+        productname = input('Digite o nome do produto: ')
+        supplierid = input('Digite o identificador do fornecedor: ')
+        categoryid = input('Digite o identificador da categoria: ')
+        quantityperunit = input('Digite a quantidade de produtos por embalagem: ')
+        unitprice = input('Digite o preço do produto: ')
+        unitsinstock = input('Digite a quantidade de unidade no estoque: ')
+        unitsonorder = input('Digite a quantidade de unidades disponiveis para venda: ')
+        reorderlevel = input('Digite nivel do produto: ')
+        discontinued = input('O produto esta descontinuado?: ')
+        valores = [productid, productname, supplierid, categoryid, quantityperunit, unitprice, 
+        unitsinstock, unitsonorder, reorderlevel , discontinued]
         return valores
 
-    def coletaDadosPedidoUpdate(self):
-        atributos = {1: 'orderid', 2: 'customerid', 3: 'employeeid', 4: 'requireddate', 5: 'shippedate', 6: 'freight', 7: 'shipname',
-            8: 'shipaddress', 9: 'shipcity', 10: 'shipregion', 11: 'shippostalcode', 12: 'shipcountry', 13: 'shipperid'}
+    def recebeCodproduto(self):
+        productid = input('Digite o identificador do produto: ')
+        return productid
+
+    def coletaMod(self):
+        productid = input('Digite o indentificador do produto: ')
+        campo = input('Digite o campo que deseja alterar: ')
+        newValue = input('Digite o novo valor: ')
+        valores = [productid, campo, newValue]
+        return valores
+
+    def imprimeProduto(self, prod):
+        if prod is not None:
+            print(f'Id: {prod.id}')
+            print(f'Nome: {prod.nome}')
+            print(f'Fornecedor: {prod.fornecedor}')
+            print(f'Categoria: {prod.categoria}')
+            print(f'Quantidade por embalagem: {prod.quantidadeEmbalagem}')
+            print(f'Preço Unitario: {prod.precoUnitario}')
+            print(f'Estoque: {prod.estoque}')
+            print(f'Vendas: {prod.vendas}')
+            print(f'Nivel: {prod.nivel}')
+            print(f'Descontinuado: {prod.descontinuado}\n\n')
+            
+
+        else:
+            print('Produto não encontrado!')
+
+    def imprimeStatus(self, string):
+        print(string)
+
+view = View()
